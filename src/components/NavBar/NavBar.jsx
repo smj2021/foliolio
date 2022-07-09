fb97175eec79dc48646da47a3f91450826a0898b
smj2021 <mjschwartz515@gmail.com>
2022-07-09 13:42:29 -0400

    first commit: created app and added basics of navbar component

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import './navbar.css';
+import { HashLink } from 'react-router-hash-link';
+
+
+const NavBar = () => {
+  return (
+    <nav>
+      <HashLink className=""></HashLink>
+    </nav>
+
+  )
+}
+
+export default NavBar;
