6c6f33ca519dca89bc6e39d7045c07666e731f68
smj2021 <mjschwartz515@gmail.com>
2022-07-09 14:02:54 -0400

    NavBar jsx skeleton complete with scrolling HashLinks

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,9 +6,14 @@ const NavBar = () => {
   return (
     <nav>
       {/* Using HashLink we are able to create scrollable links that don't function with the standard Link component https://www.npmjs.com/package/react-router-hash-link */}
-      <HashLink className="title" smooth to="#top"></HashLink>
+      <HashLink className="title" smooth to="#top">Michael</HashLink>
+      <div>
+        <HashLink smooth to="#about">About Me</HashLink>
+        <HashLink smooth to="#skills">Languages &amp; Technologies</HashLink>
+        <HashLink smooth to="#projects">Projects</HashLink>
+        <HashLink smooth to="#contact">Contact Me</HashLink>
+      </div>
     </nav>
-
   )
 }
 
