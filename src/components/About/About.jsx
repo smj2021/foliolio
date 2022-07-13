88dfb645610435e6cae1985c4d583de5532b433c
smj2021 <mjschwartz515@gmail.com>
2022-07-13 16:58:35 -0400

    adding body to about.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { about } from '../../portfolio';
 import './about.css';
 
 const About = () => {
+  // Destructuring about variable so I can call props throughout jsx
+  const { name, role, description, resume, social } = about
+
   return (
     <section className="about-me" id="about">
       <div>
-        <h1>Hi, I'm Michael</h1>
+        {name && (
+          <h1>
+            Hi, I&apos;m <span className='about__name'>{name}.</span>
+          </h1>
+        )}
+
+        {role && <h2 className='about__role'>A {role}.</h2>}
+        <p className='about__desc'>{description && description}</p>
+
+
+
       </div>
     </section>
   )
