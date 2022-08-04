96b70ea3a95fc975b4ed089cd89bff62f55d10a7
smj2021 <mjschwartz515@gmail.com>
2022-08-03 20:15:30 -0400

    added footer component with github link

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { footer } from '../../portfolio.js'
+
+const Footer = () => {
+  <footer className="footer">
+    <a
+      href="https://github.com/smj2021/foliolio"
+      className="link footer__link"
+    >
+      Created by Michael Schwartz
+    </a>
+  </footer>
+}
+
+export default Footer;
